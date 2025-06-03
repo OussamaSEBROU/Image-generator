fcec44ee7186504a075b9c79fc22c3969514210b
Mr. Oussama Sebrou <90466380+OussamaSEBROU@users.noreply.github.com>
2025-06-04 00:20:16 +0200

    Create src/index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Get the root element from index.html
+const rootElement = document.getElementById('root');
+
+// Create a root and render the App component
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
